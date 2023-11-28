6c6d994935f96f3c323e6d58965ab5f071562737
Farid <faridbusiness@bk.ru>
2023-11-28 10:51:49 +0100

    Added domains

---
+++ W.O.API/Domain/Part.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Core;
+using System.ComponentModel.DataAnnotations.Schema;
+using W.O.API.Domain.Common;
+using W.O.API.Domain.Common.Exceptions;
+using W.O.API.Domain.Common.Helpers;
+
+namespace W.O.API.Domain
+{
+    [Table("Parts")]
+    public class Part : Base
+    {
+        public Part(Guid visitId, string description, Price price, int quantity)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(description, nameof(description));
+
+            this.VisitId = visitId;
+            this.Description = description;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        private Part() { }
+
+        public Guid VisitId { get; private set; }
+        public string Description { get; private set; }
+        public int Quantity { get; private set; }
+
+        [NotMapped]
+        public Price Price { get; private set; }
+
+        [NotMapped]
+        public Price TotalPrice => new Price { Currency = this.Price.Currency, Amount = this.Price.Amount * Quantity };
+
+        public Part Update(string? description, decimal? amount, string? currency, int? quantity)
+        {
+            Description = description ?? this.Description;
+            Price.Amount = amount ?? this.Price.Amount;
+            Price.Currency = currency != null
+                ? CurrencyHelper.GetFromString(currency)
+                : this.Price.Currency;
+            Quantity = quantity ?? this.Quantity;
+
+            return this;
+        }
+    }
+}
+
---
+++ W.O.API/Domain/WorkOrder.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using W.O.API.Domain.Common;
+using W.O.API.Domain.Common.Exceptions;
+
+
+namespace W.O.API.Domain
+{
+    public class WorkOrder : Base
+    {
+        public WorkOrder(string title, string description, string phone, string email, DateTime startAt, DateTime finishAt)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(title, nameof(title));
+            ArgumentException.ThrowIfNullOrEmpty(description, nameof(description));
+            ArgumentException.ThrowIfNullOrEmpty(phone, nameof(phone));
+            ArgumentException.ThrowIfNullOrEmpty(email, nameof(email));
+            CustomArgumentException.ThrowIfDefault(startAt, nameof(startAt));
+            CustomArgumentException.ThrowIfDefault(finishAt, nameof(finishAt));
+            
+
+            Title = title;
+            Description = description;
+            Phone = phone;
+            Email = email;
+            StartAt = startAt;
+            FinishAt = finishAt;
+        }
+
+        private WorkOrder() { }
+
+        public string Title { get; private set; } = null!;
+        public string Description { get; private set; } = null!;
+        public string Phone { get; private set; }
+        public string Email { get; private set; }
+        public DateTime StartAt { get; private set; }
+        public DateTime FinishAt { get; private set; }
+
+        [NotMapped]
+        public int TotalVisits => this.Visits.Count;
+
+        [NotMapped]
+        public int TotalParts
+        {
+            get
+            {
+                int x = 0; foreach (var visit in this.Visits)
+                {
+                    x += visit.TotalParts;
+                }
+                return x;
+            }
+        }
+
+        public ICollection<Visit> Visits { get; private set; } = new List<Visit> { };
+
+        public WorkOrder Update(string? title, string? description, string? phone, string? email, DateTime? startAt, DateTime? finishAt)
+        {
+            Title = title ?? this.Title;
+            Description = description ?? this.Description;
+            Phone = phone ?? this.Phone;
+            Email = email ?? this.Email;
+            StartAt = startAt ?? this.StartAt;
+            FinishAt = finishAt ?? this.FinishAt;
+
+            return this;
+         }
+        }
+    }
+
