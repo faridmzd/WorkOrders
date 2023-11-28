24cc51d716c061e01a64797a46556932d4b53390
Farid <faridbusiness@bk.ru>
2023-11-28 10:53:09 +0100

    helper methods added

---
+++ W.O.API/Domain/Common/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Core;
+
+namespace W.O.API.Domain.Common.Helpers
+{
+    public static class CurrencyHelper
+    {
+        public static string[] GetCurrencyNames() 
+        {
+            return Enum.GetNames(typeof(Currency));
+        }
+
+        public static Currency GetFromString(string value)
+        {
+            return (Currency)Enum.Parse(typeof(Currency), value, true);
+        }
+
+    }
+}
