040411c1e3272514e16b1c2625a0689580747ac0
Farid <faridbusiness@bk.ru>
2023-11-28 10:52:36 +0100

    Added data, repos

---
+++ W.O.API/Data/Repositories/Concrete/PartRepository.cs
@@ -0,0 +1,14 @@
+﻿using W.O.API.Data.Repositories.Abstract;
+using W.O.API.Domain;
+
+namespace W.O.API.Data.Repositories.Concrete
+{
+    public class PartRepository : BaseRepository<Part>, IPartRepository
+    {
+        public PartRepository(AppDBContext context) : base(context)
+        {
+
+        }
+    }
+}
+
---
+++ W.O.API/Data/Repositories/Concrete/VisitRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using W.O.API.Data.Repositories.Abstract;
+using W.O.API.Domain;
+
+namespace W.O.API.Data.Repositories.Concrete
+{
+    public class VisitRepository : BaseRepository<Visit>, IVisitRepository
+    {
+        public VisitRepository(AppDBContext context) : base(context) {}
+
+        public override async Task<List<Visit>> GetAllAsync()
+        {
+           return await _entities.Include(v => v.Parts).ToListAsync() ?? new List<Visit>();
+        }
+
+        public override async Task<Visit?> GetByIdAsync(Guid id)
+        {
+            return await _entities.Include(x => x.Parts).FirstOrDefaultAsync(v => v.Id == id);
+        }
+    }
+}
+
